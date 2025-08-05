7e122a9050003bb71c0bbde522052708a9acc579
Lu <132826227+LuDreamer@users.noreply.github.com>
2025-08-05 12:50:13 +0800

    Revert "8.4"
    This reverts commit 0aed9380ccb0aa2d927af65348de4a78f1bc0dd7.

--- JS基础-面试题/8.5/Object.is的手写.js
+++
@@ -1,9 +0,0 @@
-function is(x,y) {
-    if(x===y){
-        return x!==0||y!==0||1/x===1||1/y===0
-        // //运行到1/x === 1/y的时候x和y都为0，但是1/+0 = +Infinity， 1/-0 = -Infinity, 是不一样的
-    }else{
-        return x!==x&&y!==y//两个NaN，返回true
-    }
-    
-}
